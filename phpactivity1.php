303d2cf73eee3a1ba475dbb5363b65a76ab8d5fc
parejustine <parejustine001@gmail.com>
2024-11-10 16:02:44 +0800

    Successfully Created vendo (2)

---
+++ phpactivity1.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Peys App</title>
+</head>
+
+<body>
+
+    <h1>Peys App</h1>
+
+    <?php
+
+    $size = isset($_POST['size']) ? intval($_POST['size']) : 60; 
+    $borderColor = isset($_POST['borderColor']) ? $_POST['borderColor'] : '#000000';  
+
+    $canvasWidth = $size;
+    $canvasHeight = $size;
+    
+    ?>
+
+    <form id="settingsForm" method="post">
+        <label for="size">Select Photo Size:</label>
+
+        <input type="range" id="sizeSlider" name="size" min="10" max="100" value="<?php echo $size; ?>" step="10"
+            oninput="this.nextElementSibling.value = this.value">
+
+        <output><?php echo $size; ?></output>
+        
+        <br><br>
+
+        <label for="borderColor">Select Border Color:</label>
+        <input type="color" id="borderColor" name="borderColor" value="<?php echo $borderColor; ?>">
+        <br><br>
+
+        <button type="submit" name="submit">Process</button>
+        <br><br>
+
+        <img id="sourceImage" src="face.jpg" alt="Image for Canvas" hidden>
+
+        <div
+            style="border:3px solid <?php echo $borderColor; ?>; width: <?php echo $canvasWidth; ?>px; height: <?php echo $canvasHeight; ?>px; overflow:hidden;">
+
+            <img src="images/face.jpg" alt="Image for Canvas" width="<?php echo $canvasWidth; ?>"
+                height="<?php echo $canvasHeight; ?>">  
+        </div>
+    </form>
+</body>
+</html>
